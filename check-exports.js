28f6440467d62630c89277ef42ea310e15ab447a
Noel Fernandes <truthmedianetworks@gmail.com>
2025-08-03 20:21:43 +0530

    chore: remove outdated and unused files for repo cleanup
    - Remove outdated test files (test-exports.js, test-icon-component.tsx, test-icons.js, check-exports.js)
    - Remove old database initialization files (init-sqlite.js, update-categories.js)
    - Remove duplicate CI/CD workflow file (.github/workflows/ci-cd.yml)
    - Remove duplicate health check file (server/monitoring/healthCheck.ts)
    - Remove test invitation image file (test-invitation.png)
    
    This cleanup removes files that are no longer referenced or needed based on the current project structure.

--- check-exports.js
+++
@@ -1,4 +0,0 @@
-import * as invitationService from './server/invitationService.ts';
-
-console.log('Available exports:');
-console.log(Object.keys(invitationService));
--- test-exports.js
+++
@@ -1,6 +0,0 @@
-import { generateInvitationPDF, downloadInvitation, ensureTempDir } from './server/invitationService.ts';
-
-console.log('Exports loaded successfully:');
-console.log('generateInvitationPDF:', typeof generateInvitationPDF);
-console.log('downloadInvitation:', typeof downloadInvitation);
-console.log('ensureTempDir:', typeof ensureTempDir);
